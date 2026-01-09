f792552eef03f2e7371ae2551c4ea000a03451eb
xzy2022 <2889354227@qq.com>
2026-01-09 13:59:56 +0800

    [02]成功启动main_new.py

--- src/config/__init__.py
+++ src/config/__init__.py
@@ -6,12 +6,16 @@ from .settings import (
     load_llm_settings,
     save_llm_settings,
     LLMProviderConfig,
-    get_default_providers
+    get_default_providers,
+    get_env_api_key,
+    get_uav_api_key
 )
 
 __all__ = [
     'load_llm_settings',
     'save_llm_settings',
     'LLMProviderConfig',
-    'get_default_providers'
+    'get_default_providers',
+    'get_env_api_key',
+    'get_uav_api_key'
 ]
